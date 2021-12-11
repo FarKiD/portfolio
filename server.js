0ee5cc737dce9d44fa2d2417453a1719e0b9529f
FarKiD <s.ostadebrahim@kandaidea.com>
2021-12-11 17:29:40 +0330

    installed react templating engine for express

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
+const path = require('path');
+
 const express = require('express');
 const app = express();
 const port = 3000;
 
-/* app.set('view engine', 'ejs'); */
+const errorController = require('./controllers/error');
+
+app.set('view engine', 'jsx');
+app.engine('jsx', require('express-react-views').createEngine());
 
 // static path
-const serve = express.static('public/dist');
+const serve = express.static(path.join(__dirname, 'public', 'dist'));
 app.use(serve);
 
 // route
@@ -13,8 +18,10 @@ app.get('/', (req, res, next) => {
   res.sendFile('index.html');
 });
 
-app.use((req, res, next) => {
-  res.status(404).send('404 page not found');
-});
+// app.use((req, res, next) => {
+//   res.status(404).send('404 page not found');
+// });
+
+app.use(errorController.pageNotFound);
 
 app.listen(port);
---
+++ views/404.jsx
@@ -0,0 +1,11 @@
+const React = require('react');
+
+function PageNotFound() {
+  return (
+    <div>
+      Page not found!
+    </div>
+  );
+};
+
+export default PageNotFound;
