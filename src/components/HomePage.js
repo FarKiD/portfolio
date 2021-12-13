06f655a6d805de1875cd864a188208b16fd3c978
FarKiD <s.ostadebrahim@kandaidea.com>
2021-12-13 18:16:45 +0330

    Adjusted font sizes to be responsive using clamp -added more base colors. -changed hover behaviour in header items. -changed the homepage structor, however it's still incomplete.

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -12,49 +12,35 @@ const HomePage = () => {
               am I?
             </h2>
           </div>
-          <div className="col">
+          <div className="col-4">
             <p className="intro__paragraph">
-              I am a full-stack developer
-              proficient with many modern technologies.
-              I started my programming career in 2017 and have
-              come a long way since then.
+              My name is Sajjad Ostadebrahim
               <br />
-              The project in front of you was made to
-              showcase my vast skillset. this is my most recent work
-              and so far the most professional one too.
+              I'm a Full-Stack Developer.
             </p>
             <a className="intro__link" href="#">
-              Read More About Me...
+              Read More About Me
             </a>
           </div>
+          <div className="col-4">
+            <p className="intro__paragraph intro__paragraph--right">
+              A full-stack web application that showcase
+              <br />
+              pretty much my entire profession.
+            </p>
+            <a className="intro__link intro__link--right" href="#">
+              Read More About It
+            </a>
+          </div>
+          <div className="col-2">
+            <h2 className="intro__title intro__title--right">
+              what <br />
+              is this?
+            </h2>
+          </div>
         </div>
-        <div className="lists row">
-          <section className="col-6">
-            <div className="left">
-              <h3 className="left__title">
-                Here is a list of my projects
-              </h3>
-              <a className="left__link" href="#">Project 1</a>
-              <a className="left__link" href="#">Project 2</a>
-              <a className="left__link" href="#">Project 3</a>
-              <a className="left__link" href="#">Project 4</a>
-              <a className="left__link" href="#">Project 5</a>
-              <a className="left__see-all" href="#">See All</a>
-            </div>
-          </section>
-          <section className="col-6">
-          <div className="right">
-              <h3 className="right__title">
-                Here is a list of my abilities and skills
-              </h3>
-              <a className="right__link" href="#">Project 1</a>
-              <a className="right__link" href="#">Project 2</a>
-              <a className="right__link" href="#">Project 3</a>
-              <a className="right__link" href="#">Project 4</a>
-              <a className="right__link" href="#">Project 5</a>
-              <a className="right__see-all" href="#">See All</a>
-            </div>
-          </section>
+        <div className="row">
+
         </div>
       </main>
     </div>
