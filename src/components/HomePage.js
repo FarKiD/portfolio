fc2baafea5ff893bbd63f475dbb7fd547390eed2
FarKiD <s.ostadebrahim@kandaidea.com>
2021-12-14 19:00:16 +0330

    Worked on the homepage's intro pretty much finished the top part. put the idea down for the second part (outro). used some mixins to thin out the index stylesheet.

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -6,7 +6,7 @@ const HomePage = () => {
     <div className="container-fluid">
       <main>
         <div className="intro row">
-          <div className="col-2">
+          <div className="col-2 no-pad">
             <h2 className="intro__title">
               who <br />
               am I?
@@ -24,9 +24,32 @@ const HomePage = () => {
           </div>
           <div className="intro__banner col"></div>
         </div>
-        <div className="row">
-          
+        <div className="second row">
+          <div className="second__title col-8">
+            <h2>
+              what is <br />
+              this?
+            </h2>
+          </div>
+          <div className='second__text col'>
+            <p className='second__paragraph'>
+              This is my Portfolio
+              <br />
+              A modern and comprehensive project.
+            </p>
+            <a className='second__link' href='#'>
+              Read More About It
+            </a>
+          </div>
+          <div className='second__banner col'></div>
         </div>
+        <section>
+          <div className='outro row'>
+            <div className='outro__github col'>
+              The project's github
+            </div>
+          </div>
+        </section>
       </main>
     </div>
   </>
