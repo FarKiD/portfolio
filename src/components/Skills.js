3cdbf577174b6b439cb5d5cf066cc81cfb234c55
FarKiD <s.ostadebrahim@kandaidea.com>
2021-12-31 18:12:13 +0330

    Development on skills page. -Laid down some of the skills page structure -Added custom font-size classes.

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -17,7 +17,82 @@ class Skills extends React.Component {
             <h2 className='front--title h1'>
               Front-End
             </h2>
-            <div className='front--border'></div>
+            <div className='row'>
+              <div className='col front--skill'>
+                <div className='front--skill--title'>
+                  <h3>
+                    Markup Languages
+                  </h3>
+                </div>
+                <div className='front--skill--body'>
+                  <h4 className='p'>
+                    HTML
+                  </h4>
+                  <h4 className='p'>
+                    XML
+                  </h4>
+                </div>
+              </div>
+              <div className='col front--skill'>
+                <div className='front--skill--title'>
+                  <h3>
+                    Styling
+                  </h3>
+                </div>
+                <div className='front--skill--body'>
+                  <h4 className='p'>
+                    CSS
+                  </h4>
+                  <h4 className='p'>
+                    SCSS
+                  </h4>
+                  <h4 className='p'>
+                    Bootstrap
+                  </h4>
+                </div>
+              </div>
+              <div className='col front--skill'>
+                <div className='front--skill--title'>
+                  <h3>
+                    Scripting
+                  </h3>
+                </div>
+                <div className='front--skill--body'>
+                  <h4 className='p'>
+                    JavaScript
+                  </h4>
+                </div>
+              </div>
+            </div>
+            <div className='row'>
+              <div className='col front--skill'>
+                <div className='front--skill--title'>
+                  <h3>
+                    Other Skills
+                  </h3>
+                </div>
+                <div className='front--skill--body'>
+                  <h4 className='p'>
+                    React.js
+                  </h4> 
+                  <h4 className='p'>
+                    Webpack
+                  </h4> 
+                  <h4 className='p'>
+                    NPM and Yarn
+                  </h4> 
+                  <h4 className='p'>
+                    React Router
+                  </h4> 
+                  <h4 className='p'>
+                    Redux
+                  </h4>
+                  <h4 className='p'>
+                    Git, Github, Gitlab
+                  </h4>
+                </div>
+              </div>
+            </div>
           </section>
           <section className=''>
             <h2 className='h1'>
