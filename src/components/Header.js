058ab5d67796acf0ea39cd09c5cc16ef09ee5a56
FarKiD <s.ostadebrahim@kandaidea.com>
2022-01-02 21:58:33 +0330

    Installed bootstrap icons, and some work on header. -Added a language selector to the header, it's still incomplete and the dropdown needs to be implemented.

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,10 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import { rmvClassActive, isActive, onLoadActive } from "../js/header.js";
+import 'bootstrap-icons/font/bootstrap-icons';
+import flagGB from '../images/gb.svg';
+import flagJP from '../images/jp.svg';
+import flagIR from '../images/ir.svg';
 
 class Header extends React.Component {
   componentDidMount() {
@@ -16,6 +20,18 @@ class Header extends React.Component {
           <Link onClick={(e) => isActive(e, '/skills')} className="navigation--item" to="/skills">Skills</Link>
           <Link onClick={(e) => isActive(e, '/about')} className="navigation--item" to="/about">About Me</Link>
           <Link onClick={(e) => isActive(e, '/contact')} className="navigation--item" to="/contact">Contact</Link>
+          <div className='navigation--language'>
+             <div className='navigation--language--container'>
+                <button onClick={(e) => langDropDown(e)} className='navigation--language--dropbtn'>
+                  <img className='navigation--language--flag' src={flagGB} />
+                  <i class="navigation--language--chevron bi bi-chevron-down"></i>
+                </button>
+                <div id='languageDropdown' className='navigation--language--dropdown'>
+                  <img src={flagIR} />
+                  <img src={flagJP} />
+                </div>
+             </div>
+          </div>
           <div className='navigation--title-container'>
             <Link onClick={() => rmvClassActive()} className="navigation--title" to="/">Home</Link>
           </div>
