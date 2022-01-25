ded43704fec8f93a88aa56590b17532391fb3c89
FarKiD <s.ostadebrahim@kandaidea.com>
2022-01-25 21:25:45 +0330

    Moved a few rules from index to base css. - Compressed the images. - Started working on the about page. - Minor changes to the header and top of the page margin.

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,38 @@
 import React from 'react';
 
+import "../styles/about.scss";
+
+import mySelfPic from '../images/myself.png';
+
 const About = () => {
   return(
     <>
-      <h1>
-        about me
-      </h1>
+      <div className='container-fluid'>
+        <main>
+          <section className='profile row'>
+            <div className='profile__image__container'>
+              <img className='profile__image' src={mySelfPic} />
+            </div>
+            <div className='profile__info'>
+              <p>
+                <strong>Name: </strong>Sajjad Ostadebrahim
+              </p>
+              <p>
+                <strong>Birthdate: </strong>25 Nov. 1998
+              </p>
+              <p>
+                <strong>Location: </strong>Tehran, Shahriyar
+              </p>
+              <p>
+                <strong>Marital Status: </strong>Single
+              </p>
+              <p>
+                <strong>Development Experience: </strong>+3 Years
+              </p>
+            </div>
+          </section>
+        </main>
+      </div>
     </>
   );
 };
