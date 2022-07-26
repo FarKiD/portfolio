5ae7a3dc732d25b3b2a10affa8ae04f64156c03c
FarKiD <sajjad.eros@gmail.com>
2022-07-26 17:25:15 +0430

    The general layout for projects is done, images are added.

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,40 +2,45 @@ import React from 'react';
 
 import "../styles/projects.scss";
 
-const Projects = () => {
-  return(
-    <>
-      <div className='container-fluid'>
-        <div className='row kanda'>
-          <div className='col-3'>
-            Image goes here
+import kandaPic from '../images/kanda.jpg';
+import shopPic from '../images/shop.jpg';
+
+class Projects extends React.Component {
+  render() {
+    return(
+      <>
+        <div className='container-fluid'>
+          <div className='row kanda'>
+            <div className='col-3'>
+              <img className='kanda__image img-fluid' src={kandaPic} />
+            </div>
+            <div className='col'>
+              Content here
+            </div>
           </div>
-          <div className='col'>
-            Content here
+          <div className='row portfolio'>
+            <div className='col-3'>
+              <img className='portfolio__image img-fluid' src={shopPic} />
+            </div>
+            <div className='col'>
+              Content here
+            </div>
+          </div>
+          <div className='row others'>
+            <div className='col'>
+              Content here
+            </div>
+            <div className='col'>
+              Content here
+            </div>
+            <div className='col'>
+              Content here
+            </div>
           </div>
         </div>
-        <div className='row portfolio'>
-          <div className='col-3'>
-            Image goes here
-          </div>
-          <div className='col'>
-            Content here
-          </div>
-        </div>
-        <div className='row others'>
-          <div className='col'>
-            Content here
-          </div>
-          <div className='col'>
-            Content here
-          </div>
-          <div className='col'>
-            Content here
-          </div>
-        </div>
-      </div>
-    </>
-  );
+      </>
+    );
+  };
 };
 
 export default Projects;
