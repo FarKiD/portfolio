b29f01543088e617fe67040db9a90a02f687a253
FarKiD <sajjad.eros@gmail.com>
2022-07-27 18:12:01 +0430

    Added the initial content for the main two projects.

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -21,7 +21,21 @@ class Projects extends React.Component {
               <img className='kanda__image img-fluid' src={kandaPic} />
             </div>
             <div className='col'>
-              Content here
+              <h3>
+                Kanda Idea
+              </h3>
+              <p>
+                Kanda Idea is a respectable GIS and technology based company.
+                in 2021 I had the great opportunity to work together with their development team and I was tasked with the 
+                Front-End of their new website. I got to work alongside their talented design team and understanding co-ordinators,
+                it was a great experience for me.
+                <br />
+                One of the toughests challenges was the large sitemap, we created around 20 pages for the website. some of which are private or unused in the main website.
+                <br />
+              </p>
+              <a href='https://kandaidea.com/'>Kanda Idea</a>
+              <span>Keywords: Front-End, Pure Javascript, Responsive Design</span>
+              <span><small>Disclaimer: The website has had small changes since I worked on it. I can provide my own version if needed.</small></span>
             </div>
           </div>
           <div className='row portfolio'>
@@ -29,7 +43,18 @@ class Projects extends React.Component {
               <img className='portfolio__image img-fluid' src={shopPic} />
             </div>
             <div className='col'>
-              Content here
+              <h3>
+                Online Shop Project
+              </h3>
+              <p>
+                Shortly after my departure with Kanda Idea, I focused on learning Back-End,
+                and this project was the ultimate result of that.
+                <br />
+                it pretty much is a back-end skill showcase.
+              </p>
+              <a href='https://ostadebrahim-shop.herokuapp.com/'>Online Shop</a>
+              <span>Keywords: Back-End, Authentication, Database</span>
+              <span><small>Disclaimer: Since using Heroku, the website might not work it's full potential.</small></span>
             </div>
           </div>
           <div className='row others'>
