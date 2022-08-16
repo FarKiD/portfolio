c9f81875cdf839a4efbe611b1aef2aea9b79edd0
FarKiD <sajjad.eros@gmail.com>
2022-08-16 16:45:40 +0430

    Removed an old and problematic module called express-react-views

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const errorController = require('./controllers/error');
 // app.set('views', path.join(__dirname, 'public', 'dist'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jsx');
-app.engine('jsx', require('express-react-views').createEngine());
+// app.engine('jsx', require('express-react-views').createEngine());
 
 // static path
 const root = path.join(__dirname, 'public', 'dist');
